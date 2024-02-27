dd7dc9319c616dc6449f5642c392fdb4fe9a1e57
akshit1098 <saxena.aks@northeastern.edu>
2024-02-27 18:22:27 -0500

    Added Permissions screen for requesting Microphone and Transcribe access

--- Transcriber/Transcriber/TranscriptionTableTableViewController.swift
+++ Transcriber/Transcriber/TranscriptionTableTableViewController.swift
@@ -6,9 +6,17 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
 
 class TranscriptionTableTableViewController: UITableViewController {
-
+    
+    
+    
+    var dummyItems: [String] = ["g", "r", "a", "n", "t"]
+    
+    var reuseIdentifier = "transcriptionsTableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,29 +25,29 @@ class TranscriptionTableTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        checkPermissions()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return dummyItems.count
     }
 
-    /*
+ 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
 
-        // Configure the cell...
-
+        cell.textLabel?.text = dummyItems[indexPath.row]
         return cell
     }
-    */
+  
 
     /*
     // Override to support conditional editing of the table view.
@@ -85,5 +93,28 @@ class TranscriptionTableTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Permissions
+    func checkPermissions(){
+        
+        let recAuthorized = AVAudioApplication.shared.recordPermission == .granted
+
+        let transAuthorized = SFSpeechRecognizer.authorizationStatus() == .authorized
+        
+        let authorised = recAuthorized && transAuthorized
+        if !authorised{
+            if let vc = self.storyboard?.instantiateViewController(withIdentifier: "PermissionsVc"){
+                self.navigationController?.present(vc, animated: true, completion: nil)
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
--- Transcriber/Transcriber/ViewController.swift
+++ Transcriber/Transcriber/ViewController.swift
@@ -6,12 +6,67 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    @IBAction func buttonClicked(_ sender: Any) {
+        requestRecordPermissions()
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //bubbles through to transcribe eprmissions
+    func requestRecordPermissions(){
+        AVAudioApplication.requestRecordPermission { [unowned self] allowed in
+            DispatchQueue.main.async{
+                if allowed{
+                    self.requestTranscribePermissions()
+                }else{
+                    self.showError()
+                }
+            }
+        }
+    }
+    
+    func requestTranscribePermissions(){
+        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async{
+                if authStatus == .authorized{
+                    //great to go
+                    self.dismiss(animated: true, completion: nil)
+                    
+                }else{
+                    //error handling
+                    self.showError()
+                }
+            }
+        }
+    }
+    
+    func showError(){
+        
+        self.label.text = "You have previously denied this app access to speech recognition. Please change the settings and restart the app!!!"
+        self.disableButton()
+        
+    }
+    
+    func disableButton(){
+        self.button.isEnabled = false
+        UIView.animate(withDuration: 1.0){
+            self.button.alpha = 0.3
+        }
     }
 
 
